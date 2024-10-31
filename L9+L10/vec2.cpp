6116c5d10f790fa512808e89568ae9f039647736
AbuAhamedRafi <abuahamedrafi@gmail.com>
2024-11-01 00:16:49 +0600

    vectors finished

---
+++ L9+L10/vec2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vec(5, 0);
+    for (int val : vec)
+    {
+        cout << val << endl;
+    }
+
+    return 0;
+}
